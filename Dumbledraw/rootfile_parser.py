f2dd00a67ca995c63f9f03eb2694eed42af86ac3
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2018-11-26 14:55:36 +0100

    break API since we have to support era tags for combination of datacards

--- Dumbledraw/rootfile_parser.py
+++ Dumbledraw/rootfile_parser.py
@@ -24,7 +24,7 @@ class Rootfile_parser(object):
         if mode == "standard":
             self._hist_hash = "{channel}_{category}{plottype}/{process}{unc}"
         elif mode == "CombineHarvester":
-            self._hist_hash = "htt_{channel}_{category}_13TeV{plottype}/{process}{unc}"
+            self._hist_hash = "htt_{channel}_{category}_{era}{plottype}/{process}{unc}"
         else:
             logger.fatal("Cannot detect mode to open file {}.".format(mode))
             raise Exception
@@ -34,12 +34,13 @@ class Rootfile_parser(object):
     def rootfile(self):
         return self._rootfile
 
-    def get(self, channel, category, process, syst=None):
+    def get(self, era, channel, category, process, syst=None):
         if syst != None and self._type != "control":
             logger.fatal(
                 "Uncertainty shapes are only available in control plots!")
             raise Exception
         hist_hash = self._hist_hash.format(
+            era=era,
             channel=channel,
             category=category,
             process=process,
@@ -54,8 +55,8 @@ class Rootfile_parser(object):
             "Try to access %s in %s" % (hist_hash, self._rootfilename))
         return self._rootfile.Get(hist_hash)
 
-    def get_bins(self, channel, category, process, syst=None):
-        hist = self.get(channel, category, process, syst)
+    def get_bins(self, era, channel, category, process, syst=None):
+        hist = self.get(era, channel, category, process, syst)
         nbins = hist.GetNbinsX()
         bins = []
         for i in range(nbins):
@@ -63,24 +64,24 @@ class Rootfile_parser(object):
         bins.append(hist.GetBinLowEdge(i + 1) + hist.GetBinWidth(i + 1))
         return bins
 
-    def get_values(self, channel, category, process, syst=None):
-        hist = self.get(channel, category, process, syst)
+    def get_values(self, era, channel, category, process, syst=None):
+        hist = self.get(era, channel, category, process, syst)
         nbins = hist.GetNbinsX()
         values = []
         for i in range(nbins):
             values.append(hist.GetBinContent(i + 1))
         return values
 
-    def get_values_up(self, channel, category, process, syst=None):
-        hist = self.get(channel, category, process, syst)
+    def get_values_up(self, era, channel, category, process, syst=None):
+        hist = self.get(era, channel, category, process, syst)
         nbins = hist.GetNbinsX()
         values = []
         for i in range(nbins):
             values.append(hist.GetBinErrDown(i + 1))
         return values
 
-    def get_values_down(self, channel, category, process, syst=None):
-        hist = self.get(channel, category, process, syst)
+    def get_values_down(self, era, channel, category, process, syst=None):
+        hist = self.get(era, channel, category, process, syst)
         nbins = hist.GetNbinsX()
         values = []
         for i in range(nbins):
