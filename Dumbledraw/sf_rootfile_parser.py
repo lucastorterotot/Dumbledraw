a2800893fbcdf0561c19fe1c9c3bd2cc931c2544
Sebastian Brommer <sebastian.brommer@kit.edu>
2019-11-08 15:13:19 +0100

    add rootfile parser for sf files

---
+++ Dumbledraw/sf_rootfile_parser.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+import ROOT
+import copy
+logger = logging.getLogger(__name__)
+
+
+class ScaleFactor_Rootfile_parser(object):
+    def __init__(self, inputrootfilename):
+        self._rootfilename = inputrootfilename
+        self._rootfile = ROOT.TFile(self._rootfilename, "READ")
+        content = [entry.GetName() for entry in self._rootfile.GetListOfKeys()]
+        self.Nbins = len(content)
+        logger.debug("Identified {} histograms in rootfile {} ".format(len(content), inputrootfilename))
+        self._hist_hash = "{variable}_pt_eta_bins_projx_{etabin}"
+
+    @property
+    def rootfile(self):
+        return self._rootfile
+
+
+
+    def get(self, variable, etabin):
+        hist_hash = self._hist_hash.format(
+            variable=variable,
+            etabin=etabin)
+        logger.debug(
+            "Try to access %s in %s" % (hist_hash, self._rootfilename))
+        # perform check if file is available and otherwise return some dummy TH1F
+        # available_processes = [entry.GetName() for entry in self._rootfile.Get(hist_hash.split('/')[0]).GetListOfKeys()]
+        # if hist_hash.split('/')[1] in available_processes:
+        return self._rootfile.Get(hist_hash)
+        # elif len(available_processes) != 0:
+        #     logger.warning("%s in %s does not exist !" % (hist_hash, self._rootfilename))
+        #     logger.debug(" Available Histograms are: %s" % available_processes)
+        #     logger.debug(" Returning a dummy histogram ")
+        #     dummy = self._rootfile.Get('{}/{}'.format(hist_hash.split('/')[0],available_processes[0]))
+        #     dummy.Reset()
+        #     dummy.SetTitle("process")
+        #     dummy.SetName(hist_hash)
+        #     return dummy
+        # else:
+        #     logger.fatal(" None of the requested Histograms are available in %s. Aborting." % hist_hash.split('/')[0])
+        #     raise Exception
+
+
+    def get_bins(self, variable, etabin):
+        hist = self.get(self, variable, etabin)
+        nbins = hist.GetNbinsX()
+        bins = []
+        for i in range(nbins):
+            bins.append(hist.GetBinLowEdge(i + 1))
+        bins.append(hist.GetBinLowEdge(i + 1) + hist.GetBinWidth(i + 1))
+        return bins
+
+    def get_values(self, variable, etabin):
+        hist = self.get(self, variable, etabin)
+        nbins = hist.GetNbinsX()
+        values = []
+        for i in range(nbins):
+            values.append(hist.GetBinContent(i + 1))
+        return values
+
+    def get_values_up(self, variable, etabin):
+        hist = self.get(self, variable, etabin)
+        nbins=hist.GetNbinsX()
+        values=[]
+        for i in range(nbins):
+            values.append(hist.GetBinErrDown(i + 1))
+        return values
+
+    def get_values_down(self, variable, etabin):
+        hist = self.get(self, variable, etabin)
+        nbins = hist.GetNbinsX()
+        values = []
+        for i in range(nbins):
+            values.append(hist.GetBinErrDown(i + 1))
+        return values
+
+    def __del__(self):
+        logger.debug("Closing rootfile %s" % (self._rootfilename))
+        self._rootfile.Close()
