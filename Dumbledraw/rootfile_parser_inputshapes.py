d38e4bebef8cf31777b023a281c0a47bff746875
Maximilian Burkart <maxiburkart@gmail.com>
2020-07-23 20:07:38 +0200

    Adapt readout of input shapes to new format of input shapes.

--- Dumbledraw/rootfile_parser_inputshapes.py
+++ Dumbledraw/rootfile_parser_inputshapes.py
@@ -4,35 +4,74 @@
 import logging
 import ROOT
 import copy
+from __future__ import print_function
 logger = logging.getLogger(__name__)
 
 
 class Rootfile_parser(object):
-    def __init__(self, inputrootfilename, analysis, epoch, variable, mass):
+
+    _dataset_map = {
+        "data": "data",
+        "ZTT": "DY",
+        "ZL": "DY",
+        "ZJ": "DY",
+        "TTT": "TT",
+        "TTL": "TT",
+        "TTJ": "TT",
+        "VVT": "VV",
+        "VVL": "VV",
+        "VVJ": "VV",
+        "W": "W",
+        "EMB": "EMB",
+        "QCDEMB": "QCD",
+        "QCD": "QCDMC",
+        "jetFakesEMB": "jetFakes",
+        "jetFakes": "jetFakesMC",
+        "ggH125": "ggH",
+        "qqH125": "qqH",
+    }
+
+    _process_map = {
+        "data": "data",
+        "ZTT": "DY-ZTT",
+        "ZL": "DY-ZL",
+        "ZJ": "DY-ZJ",
+        "TTT": "TT-TTT",
+        "TTL": "TT-TTL",
+        "TTJ": "TT-TTJ",
+        "VVT": "VV-VVT",
+        "VVL": "VV-VVL",
+        "VVJ": "VV-VVJ",
+        "W": "W",
+        "EMB": "Embedded",
+        "QCDEMB": "QCD",
+        "QCD": "QCDMC",
+        "jetFakesEMB": "jetFakes",
+        "jetFakes": "jetFakesMC",
+        "ggH125": "ggH125",
+        "qqH125": "qqH125",
+    }
+
+    def __init__(self, inputrootfilename, variable):
         self._rootfilename = inputrootfilename
         self._rootfile = ROOT.TFile(self._rootfilename, "READ")
-        self._type = "control"
-        self._analysis = analysis
-        self._epoch = epoch
         self._variable = variable
-        self._mass = mass
 
     @property
     def rootfile(self):
         return self._rootfile
 
-    def get(self, channel, category, process):
-        hist_hash = "#{channel}#{category}#{process}#{analysis}#{epoch}#{variable}#{mass}#".format(
+    def get(self, channel, process, shape_type="Nominal"):
+        dataset = self._dataset_map[process]
+        hist_hash = "{dataset}#{channel}{category}#{shape_type}#{variable}".format(
+            dataset=dataset,
             channel=channel,
-            category=category,
-            process=process,
-            analysis=self._analysis,
-            epoch=self._epoch,
-            variable=self._variable,
-            mass=self._mass)
+            category="" if "data" in process else "-" + self._process_map[process],
+            shape_type=shape_type,
+            variable=self._variable)
         logger.debug("Try to access %s in %s" % (hist_hash,
                                                  self._rootfilename))
-        print "rootfile: " , self._rootfile.Get(hist_hash), " hash: ", hist_hash
+        print("rootfile: " , self._rootfile.Get(hist_hash), " hash: ", hist_hash)
 
         return self._rootfile.Get(hist_hash)
 
